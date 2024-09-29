a1b41f796a9273930751e34b8bc7a2e1cbe3c648
MohamedWael21 <mohame171819@gmail.com>
2024-09-29 23:15:49 +0300

    modify config file of next

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
   },
